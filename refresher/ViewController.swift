151edc77b311c1ae4d6a1918d2999d794ca2b005
Lorin van Riel <lorinvanriel@gmail.com>
2019-11-25 12:26:41 +1100

    Set up table

--- refresher/ViewController.swift
+++ refresher/ViewController.swift
@@ -9,18 +9,31 @@
 import SnapKit
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
-    let label = UILabel()
+    let cellId = "simpleCell"
+    let rowCount = 15
+    let tableData: [Int] = Array(1...15)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rowCount
+    }
     
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel?.text = "\(tableData[indexPath.row])"
+        return cell
+    }
 }
 
