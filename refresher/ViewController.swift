9cf5c29a4114dd83ff860e85e37a4e50e78c573f
Lorin van Riel <lorinvanriel@gmail.com>
2019-11-25 13:45:23 +1100

    Simple table refreshing using standard UIRefreshControl. Nothing to see here.

--- refresher/ViewController.swift
+++ refresher/ViewController.swift
@@ -11,17 +11,19 @@ import UIKit
 
 class ViewController: UITableViewController {
 
-    let cellId = "simpleCell"
-    let rowCount = 15
-    let tableData: [Int] = Array(1...15)
+    private var tableData: [Int] = Array(1...15)
+    
+    private let cellId = "simpleCell"
+    private let rowCount = 15
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        buildView()
     }
 
+    // MARK:- UITableViewDataSource
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -29,11 +31,26 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return rowCount
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
         cell.textLabel?.text = "\(tableData[indexPath.row])"
         return cell
+    }
+    
+    // MARK:- Private helpers
+    private func buildView() {
+        view.backgroundColor = .white
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
+    }
+    
+    @objc private func refresh(_ sender: Any) {
+        tableData = tableData.map { i in
+            return i * 2
+        }
+        refreshControl?.endRefreshing()
+        tableView.reloadData()
     }
 }
 
